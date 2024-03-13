5cae3534a5e2f3b1f2e1a25023ac93a751b9e285
TianaxJames <itstianajames@gmail.com>
2024-03-12 21:59:08 -0400

    injected weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let description = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity-value");
+  console.log(response.data);
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind-value");
+  let wind = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
   temperatureElement.innerHTML = Math.round(temperature);
+  windElement.innerHTML = Math.round(wind);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 }
 
 function searchCity(city) {
