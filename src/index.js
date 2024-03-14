c9d8ac00db8531fe6a2a281c4832422b41093f5b
TianaxJames <itstianajames@gmail.com>
2024-03-14 13:28:22 -0400

    Injected the forecast HTML to JavaScript

--- src/index.js
+++ src/index.js
@@ -56,8 +56,34 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function displayForecast() {}
+
+let days = ["Tues", "Wed", "Thur", "Fri", "Sat"];
+let forecastHtml = "";
+
+days.forEach(function (day) {
+  forecastHtml =
+    forecastHtml +
+    `
+    <div class="weather-forecast-day"> 
+    <div class="weather-forecast-date">${day}</div>
+    <img
+    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+    alt=""
+    width="60px"
+    /> 
+    <div class="weather-forecast-temps">
+    <span class="weather-forecast-temp-max"><strong>18°</strong></span> <span class="weather-forecast-temp-min"><strong>13°</strong></span>
+    </div>
+    </div>
+    `;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Montreal");
+displayForecast();
